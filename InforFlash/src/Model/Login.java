64534a3b08f6141074debd3f74e85ba389a606aa
Caio Picelli <meiademanteiga767@gmail.com>
2025-10-14 23:12:13 -0300

    Add files via upload

---
+++ InforFlash/src/Model/Login.java
@@ -0,0 +1,35 @@
+package Model;
+
+/**
+ *
+ * @author User
+ */
+public class Login {
+    
+    private String usuario, senha;
+    private boolean loginValidado = false;
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isLoginValidado() {
+        return loginValidado;
+    }
+
+    public void setLoginValidado(boolean loginValidado) {
+        this.loginValidado = loginValidado;
+    }
+}
